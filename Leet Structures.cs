4c5ada685f2f473ade4cec81247a796231b30e5a
PabloKharo <kharkov.p.a@gmail.com>
2021-04-30 11:05:11 +0300

    Структуры для задач

---
+++ Leet Structures.cs
@@ -0,0 +1,20 @@
+public class ListNode { // Definition for singly-linked list.
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+
+public class TreeNode { // Definition for a binary tree node.
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+            this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
